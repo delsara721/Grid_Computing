be4d99ecf0ddd018fdf88b2649fec3dd72159006
delsara721 <delsara721@yahoo.com>
2017-10-18 15:07:57 -0700

    Update with more comments and renamed file

--- 1D_PDE_grid_MPI.cpp
+++ 1D_PDE_grid_MPI.cpp
@@ -1,12 +1,18 @@
+//Delsey Sabu
+//PDE solver 
+//given a domain, we break it in half to learn the process of communciation between grids as time steps increases
+//the domain is split into right and left arrays where the computaions are done separately, 
+//but left and right arrays communicate to each other at the end of each time step using MPI
+
 #include <iostream>
 #include<math.h>
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int ROWS_R =  6;
-int ROWS_L = 6; 
-const int c= 1; 
+int ROWS_R =  6; //the size of right array is 4 units, the outer indexes are left for communcation
+int ROWS_L = 6; //the size of left array is 4 units, the outer indexes are left for communcation
+const int c= 1; //constant c for teh heat equation
 int ROWS_X = 8; 
 double POSITION_MIN = -3.5; 
 double dx = 1;
@@ -28,11 +34,13 @@ int main(int argc, char** argv)
 bool Lahead = false; 
 bool Rahead = false; 
 int tag = 0; 
+	//need 4 arrays, two(right_old and left_old) for keeping values from old time step
+	//and two(right_new and left_new) for saving values from new time step
 double right_new[ROWS_R];
 double right_old[ROWS_R];  
 double left_new[ROWS_L];
 double left_old[ROWS_L];
-
+//need an array to fill the positions 
 double position[ROWS_X];
 fillPosition(position);
 //printposition(position);   
@@ -54,21 +62,25 @@ period = TIME_MAX/dt;
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
- //left_old[0] = right_old[ROWS_R-2]; 
-//left = process 0 
-    if (world_rank == 0)
+	//fill left and rigt ararys with initial values with cos(x), where x is position, time =0
+        //first process - left array 
+	if (world_rank == 0)
   {
 for (int i =1; i<ROWS_L-1; i++)
 {
+	//for every inner index in left array, fill with cos(x), where x is position  
 result = cos(position[i-1]); 
 left_old[i] = result; 
 }
+	    //send to right array their outer values
     MPI_Send(&left_old[1], 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
     MPI_Send(&left_old[ROWS_L-2], 1, MPI_DOUBLE, 1, 1, MPI_COMM_WORLD);
+	    //receive from right array the left arrays outer values
     MPI_Recv(&left_old[0], 1, MPI_DOUBLE, 1, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Recv(&left_old[ROWS_L-1], 1, MPI_DOUBLE, 1, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 printT(left_old, ROWS_L);
 }
+	//for second process: right array
  else if (world_rank == 1) 
 {
 for (int i =1; i<ROWS_R-1; i++)
@@ -76,10 +88,10 @@ for (int i =1; i<ROWS_R-1; i++)
 result = cos(position[(ROWS_R-1)+(i-1)]); 
 right_old[i] = result; 
 }
-
+//receive from left array the outer values
     MPI_Recv(&right_old[ROWS_R-1], 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     MPI_Recv(&right_old[0], 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+//send to right it's outer values
     MPI_Send(&right_old[ROWS_R-2], 1, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
     MPI_Send(&right_old[1], 1, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD);
 printT(right_old, ROWS_R); 
@@ -88,10 +100,12 @@ printT(right_old, ROWS_R);
 
 
  MPI_Finalize();
-/*
+
+	//for every time step after initialization:
 for (int i =1; i<=period+1; i++)
 {
 cout << i << endl; 
+	//fill with new values 
 fill(left_old, left_new, right_old, right_new); 
 
 cout << "LEFT" << endl; 
@@ -99,10 +113,11 @@ printT(left_new, ROWS_L);
 cout << "RIGHT" << endl; 
 printT(right_new, ROWS_R); 
 }
-*/
+
 return 0; 
 }
 
+//print array function 
 void printT(double T[], int ROWS)
 {
 for (int j =1; j<ROWS-1; j++)
@@ -111,6 +126,7 @@ for (int j =1; j<ROWS-1; j++)
 cout << endl; 
 }
 
+//fill with new values
 void fill(double left_old[],double left_new[], double right_old[],double right_new[])
 { 
 	double r = (c * dt)/(dx * dx); 
@@ -121,7 +137,7 @@ void fill(double left_old[],double left_new[], double right_old[],double right_n
 	left_new[i] = (r * left_old[i-1])+(k * left_old[i])+(r * left_old[i+1]);
 	}//end for loop
 
-for (int i =1; i<ROWS_R-1; i++)
+	for (int i =1; i<ROWS_R-1; i++)
 	{
 	right_new[i] = (r * right_old[i-1])+(k * right_old[i])+(r * right_old[i+1]);
 	}//end for loop
